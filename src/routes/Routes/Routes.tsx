6f1cb1d3ee1dbc72d62aa9a249f2c4cbcd90cd3f
Alfredo Bautista <abautista200214@gmail.com>
2023-08-17 22:57:30 -0600

    Cambios menores

--- src/routes/Routes/Routes.tsx
+++ src/routes/Routes/Routes.tsx
@@ -1,7 +1,6 @@
 import { NotFound } from '@/app/common/NotFound';
 import { Clubes } from '@/app/pages/Clubes';
 import { Home } from '@/app/pages/Home';
-import { Login } from '@/app/pages/Login';
 import { Planes } from '@/app/pages/Planes';
 import React from 'react';
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
@@ -17,8 +16,6 @@ const Rutas: React.FC<RoutesProps> = ({ }) => {
 					<Route index element={<Home />}></Route>
 					<Route path='/clubes' element={<Clubes />}></Route>
 					<Route path='/planes' element={<Planes />}></Route>
-					<Route path='/login' element={<Login />}></Route>
-
 				</Route>
 				<Route path='*' element={<NotFound />}></Route>
 			</Routes>
