7c768a33301ce0a8cb530ef895f84d8d0d75b281
Alfredo Bautista Ramírez <abautista200214@gmail.com>
2023-09-05 03:52:11 +0000

    Ahora si guardaré los cambios

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
-import Suscripciones from '@/app/components/Suscripciones/Suscripciones';
 import imagen1 from '@/assets/imageLogo5ta.jpeg';
 import imagen2 from '@/assets/imagePink.jpeg';
 import imagen3 from '@/assets/imageVilla.jpeg';
+import Suscripciones from '@/app/components/Suscripciones/Suscripciones';
 
 export type HomeProps = {
 }
@@ -11,25 +11,38 @@ export type HomeProps = {
 const Home: React.FC<HomeProps> = ({ }) => {
   const [showModal, setShowModal] = useState(false);
 
+  const [backgroundImage, setBackgroundImage] = useState(1);
+  const totalImages = 2; // Número total de imágenes disponibles
+
+  useEffect(() => {
+    // Función para cambiar la imagen de fondo cada 5 segundos y reiniciar en bucle
+    const intervalId = setInterval(() => {
+      setBackgroundImage((prevImage) => (prevImage === totalImages ? 1 : prevImage + 1));
+    }, 5000); // 5000 milisegundos (5 segundos)
+
+    // Limpia el intervalo al desmontar el componente para evitar fugas de memoria
+    return () => clearInterval(intervalId);
+  }, []);
+
 
   return <>
     <div>
-      <div className='justify-center lg:h-[35rem] md:h-[25rem] h-[15rem] items-center container-inscript'>
+      <div className={`justify-center lg:h-[35rem] md:h-[25rem] h-[15rem] items-center container-inscript bg-image-${backgroundImage} transition-all duration-1000`}>
         <div className='flex flex-col gap-2'>
-          <div className='lg:text-5xl md:text-xl text-sm text-center animated-bg-text p-2 rounded'>
+          <div className='lg:text-5xl md:text-xl text-sm text-center animated-bg-text'>
             <h1>INSCRIPCIÓN</h1>
           </div>
-          <div className='lg:text-5xl md:text-xl text-sm text-center animated-bg-text p-2 rounded'>
+          <div className='lg:text-5xl md:text-xl text-sm text-center animated-bg-text'>
             <h1>GRATUITA</h1>
           </div>
         </div>
       </div>
-      <div className="container mx-auto p-4 mt-14">
+      <div className="mx-4 p-6 mt-14">
         <div>
           <div className="mb-4 text-center">
-            <p className="lg:text-4xl md:text-2xl text-xl text-start">ESTAMOS DONDE TÚ ESTÁS...</p>
+            <p className="lg:text-4xl md:text-2xl text-xl lg:text-start font-semibold">ESTAMOS DONDE TÚ ESTÁS...</p>
           </div>
-          <div className='grid  grid-cols-2 lg:gap-8 justify-center align-middle items-center pt-5'>
+          <div className='grid lg:grid-cols-2 lg:gap-8 justify-center align-middle items-center pt-5'>
             <div className="grid aspect-auto grid-container p-4">
               <div className='image left-image'>
                 <img src={imagen1} />
@@ -42,40 +55,15 @@ const Home: React.FC<HomeProps> = ({ }) => {
               </div>
             </div>
             <div className='flex flex-col justify-center text-center gap-8'>
-              <p className='clu lg:text-5xl md:text-4xl sm:text-4xl '>6 <a href="/clubes"><span className='redtitle'>Clubes</span></a></p>
-              <p className='clu lg:text-5xl md:text-4xl sm:text-4xl '><a href="/planes">Suscripciones</a></p>
-              <p className='adq lg:text-lg md:text-base sm:text-sm '>Adquiere una suscripción <span className='redtitle'>Black Plus</span> y obtén acceso a todos los clubes</p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-      {/* <div className='section2'>
-      <div className='places mt-20'>
-        <h2 className='estamos lg:text-5xl md:text-3xl sm:text-4xl'>ESTAMOS DONDE TÚ ESTÁS...</h2>
-        <div className='container image-stack'>
-          <div className='grid-container'>
-            <div className='image image1'>
-              <img src={imagen1} />
-            </div>
-            <div className='image image2'>
-              <img src={imagen2} />
-            </div>
-            <div className='image image3'>
-              <img src={imagen3} />
+              <p className='clu lg:text-5xl md:text-4xl text-4xl lg:text-end'>6 <a href="/clubes"><span className='hover:text-red-600'>Clubes</span></a></p>
+              <p className='clu lg:text-5xl md:text-4xl text-4xl lg:text-end'><a href="/planes" className='hover:text-red-600'>Suscripciones y beneficios</a></p>
+              <p className='adq lg:text-lg md:text-base text-sm lg:text-start'>Adquiere una suscripción <span className='text-red-600'>Black Plus</span> y obtén acceso a todos los clubes</p>
             </div>
           </div>
         </div>
       </div>
-      <div className='titles-places'>
-        <p className='clu lg:text-5xl md:text-4xl sm:text-4xl '>6 <a href="/clubes"><span className='redtitle'>Clubes</span></a></p>
-        <p className='clu lg:text-5xl md:text-4xl sm:text-4xl '><a href="/planes">Suscripciones</a></p>
-        <p className='adq lg:text-lg md:text-base sm:text-sm '>Adquiere una suscripción <span className='redtitle'>Black Plus</span> y obtén acceso a todos los clubes</p>
-      </div>
-    </div> */}
 
-      {/* <Suscripciones /> */}
+      <Suscripciones />
       {/* <button className='comienza flex items-center justify-center' onClick={() => setShowModal(true)}>¡Comienza ya!</button> */}
       {showModal ? (
         <>
