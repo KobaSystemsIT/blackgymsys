2e20be2b560506c01769d0e7fdb08a18fbcd58a0
Alfredo Bautista Ramírez <abautista200214@gmail.com>
2023-09-07 01:31:11 +0000

    cambios

--- src/app/pages/Planes/Planes.tsx
+++ src/app/pages/Planes/Planes.tsx
@@ -8,23 +8,21 @@ export type PlanesProps = {
 }
 
 const ViewPlanes: React.FC<PlanesProps> = () => {
-	const [showModal, setShowModal] = useState(false);
 	const [planes, setPlanes] = useState<Planes[]>([]);
 
 	const [backgroundImage, setBackgroundImage] = useState(1);
 	const totalImages = 2;
 
-	const wordColors = {
+	const wordColors: Record<string, string> = {
 		"Week": "orange",
 		"2Week": "orange",
 		"Fit": "yellow",
 		"Year": "red",
 		"Plus": "red",
 		"Medium": "red",
-	};
+	  };
 
 	useEffect(() => {
-
 		const intervalId = setInterval(() => {
 			setBackgroundImage((prevImage) => (prevImage === totalImages ? 1 : prevImage + 1));
 		}, 5000);
@@ -59,16 +57,16 @@ const ViewPlanes: React.FC<PlanesProps> = () => {
 					</div>
 				</div>
 			</div>
-			<div className="mb-4 text-center">
+			<div className="mb-4 text-center mt-10">
 				<p className="lg:text-4xl md:text-2xl text-xl font-semibold">¡INSCRIPCIÓN GRATUITA!</p>
 			</div>
-			<div className="planes-container">
+			<div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1">
 				{planes.map((plan) => {
 					const words = plan.namePlanes.split(" ");
 					const firstWord = words.shift();
 
 					return (
-						<div key={plan.idPlanes} className="plan-card">
+						<div key={plan.idPlanes} className="plan-card lg:text-2xl md:text-xl text-lg">
 							<h2>
 								<span>{firstWord} </span>
 								{words.map((word, index) => (
@@ -83,89 +81,7 @@ const ViewPlanes: React.FC<PlanesProps> = () => {
 				})}
 			</div>
 			<div>
-				<Suscripciones />
-				{showModal ? (
-					<>
-						<div className=" mod justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-							<div className="relative w-auto my-6 mx-auto max-w-3xl">
-								{/*content*/}
-								<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none focus:outline-none">
-									{/*header*/}
-									<div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-										<h3 className="text-3xl font-semibold">
-											Contactános
-										</h3>
-										<button
-											className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-											onClick={() => setShowModal(false)}
-										>
-											<span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-												×
-											</span>
-										</button>
-									</div>
-									{/*body*/}
-									<div className="relative p-6 flex-auto">
-										<form>
-											<div className="mb-4">
-												<label className="block text-white text-sm font-bold mb-2" htmlFor="name">
-													Nombre
-												</label>
-												<input
-													type="text"
-													id="name"
-													name="name"
-													className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-													required
-												/>
-											</div>
-											<div className="mb-4">
-												<label className="block text-white text-sm font-bold mb-2" htmlFor="email">
-													Correo
-												</label>
-												<input
-													type="email"
-													id="email"
-													name="email"
-													className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
-													required
-												/>
-											</div>
-											<div className="mb-4">
-												<label className="block text-white text-sm font-bold mb-2" htmlFor="phone">
-													Teléfono
-												</label>
-												<input
-													type="tel"
-													id="phone"
-													name="phone"
-													className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-													required
-												/>
-											</div>
-											<div className="flex items-center justify-end">
-												<button
-													className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-20 mb-1 ease-linear transition-all duration-150"
-													type="button"
-													onClick={() => setShowModal(false)}
-												>
-													Cerrar
-												</button>
-												<button
-													className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-													type="submit"
-												>
-													Enviar
-												</button>
-											</div>
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-					</>
-				) : null}
+				<Suscripciones />				
 			</div>
 		</>
 	);
